d778d432f22cf122e1837c8dbc2bd04034580c61
Mario Nageh <marionageh7@gmail.com>
2023-08-15 02:54:28 +0300

    AirBnB_clone [BaseModel]

---
+++ models/base_model.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Base model module """
+
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """BaseModel class"""
+
+    def __init__(self):
+        """__init__ method"""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """__str__ method"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """save method"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        to_dict method
+
+        :returns dictionary representation
+        """
+        data = {
+            **self.__dict__
+        }
+        data["__class__"] = self.__class__.__name__
+        data["created_at"] = self.created_at.isoformat()
+        data["updated_at"] = self.updated_at.isoformat()
+        return data
