f9f84a553eb589cc4ba5254b491c95c091bf75f0
Mario Nageh <marionageh7@gmail.com>
2023-08-15 03:14:28 +0300

    Add BaseModel From Dict

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,19 @@ from datetime import datetime
 class BaseModel:
     """BaseModel class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """__init__ method"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key,
+                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """__str__ method"""
